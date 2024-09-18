f81b654d2c1979c744463523142c9b4b13619053
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2024-09-18 12:00:15 -0300

    Update index.jsx

--- src/screens/home/index.jsx
+++ src/screens/home/index.jsx
@@ -43,7 +43,12 @@ const Home = () => {
       />
       <BotaoChamado />
       <BotaoChamado />
-      <GerenciamentoChamadoModal visivel={modalCriaTarefaVisible} titulo={textoTituloModal} setVisivel={setModalCriaTarefaVisible} setTexto={setTextoTituloModal}/>
+      <GerenciamentoChamadoModal
+        visivel={modalCriaTarefaVisible}
+        titulo={textoTituloModal}
+        setVisivel={setModalCriaTarefaVisible}
+        setTexto={setTextoTituloModal}
+      />
     </View>
   );
 };
@@ -54,42 +59,4 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#F5F8FA',
   },
-  areaTextoPrincipal: {
-    backgroundColor: '#8D9CD3',
-  },
-  textoPrincipal: {
-    color: '#3C3A9B',
-    fontSize: 25,
-  },
-  formularioTarefa: {
-    flex: 1,
-  },
-  areaReclamante: {
-    margin: 10,
-  },
-  areaTipoChamado: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    marginRight: 10,
-    marginLeft: 10,
-  },
-  input: {
-    borderStyle: 'solid',
-    borderRadius: 8,
-    borderWidth: 1,
-    borderColor: '#000',
-    backgroundColor: '#DCE2E5',
-    padding: 8,
-  },
-  tipoChamado: {
-    width: 150,
-  },
-  textoInfoChamado: {
-    color: '#617480',
-    fontSize: 10,
-  },
-  areaDescricao: {
-    margin: 10
-  }
 });
